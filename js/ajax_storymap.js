eaf11c9f25171bf9aa0c66c8b4bd00c7ae12a8ef
m.ortega <m.ortega@icgc.cat>
2017-07-21 12:06:06 +0200

    Canvis

--- js/ajax_storymap.js
+++ js/ajax_storymap.js
@@ -1,56 +1,42 @@
 var urlStoryMap = "http://172.70.1.32/storymaps/"; 
 function getMapSlides(idMapa){
 	//Example: http://172.70.1.32/storymaps/768ddc60-66e2-11e7-af5e-09aff007230d
-	$.ajax({
+	return $.ajax({
 		  method: "GET",
 		  url: urlStoryMap+idMapa
-		})
-		  .done(function( msg ) {
-		    console.debug( msg );
-		  });
+		});
+		
 }
 function editMapSlides(idMapaEditing){
 	//Example: http://172.70.1.32/storymaps/76d21f00-48e0-4216-bb52-84a8a41cc6a2
-	$.ajax({
+	return $.ajax({
 		  method: "GET",
-		  url: urlStoryMap+"edit/"+idMapaEditing
-		})
-		  .done(function( msg ) {
-		    console.debug(  msg );
-		  });
+		  url: urlStoryMap+"edit/"+idMapaEditing,   
+          async: false
+    }).responseText;
+
 }
 function newMapSlides(jsonSlides){
 	//http://172.70.1.32/storymaps/edit/
-	$.ajax({
+	return $.ajax({
 		  method: "POST",
 		  url: urlStoryMap+"edit/",
 		  dataType: 'json',
 		  data: { id:0, id_editor:0, slides: jsonSlides}
-		})
-		  .done(function( msg ) {
+		});
+	/*	  .done(function( msg ) {
 		    console.debug(msg );
-		  });
+		  });*/
 }
 
 
 function updateMapSlides(idMapaEditing, idMapa,jsonSlides){
-	$.ajax({
+	return $.ajax({
 		  method: "PUT",
 		  url: urlStoryMap+"edit/"+idMapaEditing,
 		  dataType: 'json',
-		  data: { id:idMapa, id_editor:idMapaEditing, slides: jsonSlides},
-		  success: function(result) {
-		        // handle success
-			  console.debug(result);
-		    },
-		    error: function(request,msg,error) {
-		        // handle failure
-		    	console.debug(error);
-		    }
-		})
-		  .done(function( msg ) {
-		    console.debug( msg );
-		  });
+		  data: { id:idMapa, id_editor:idMapaEditing, slides: jsonSlides}
+	});
 }
 
 function deleteMapSlides(idMapaEditing,idMapa){
@@ -69,7 +55,7 @@ function testNode(){
 	//getMapSlides("768ddc60-66e2-11e7-af5e-09aff007230d");
 	//editMapSlides("76d21f00-48e0-4216-bb52-84a8a41cc6a2");
 	//newMapSlides(JSON.stringify(slides));
-	updateMapSlides("76d21f00-48e0-4216-bb52-84a8a41cc6a2","768ddc60-66e2-11e7-af5e-09aff007230d",slides);
+	//updateMapSlides("76d21f00-48e0-4216-bb52-84a8a41cc6a2","768ddc60-66e2-11e7-af5e-09aff007230d",slides);
 	//getMapSlides("d7139d30-6887-11e7-86f3-89afdd5a3d46");
 	//deleteMapSlides("12345","2");
 }
